3fe0425aefee72d09fa7d0c1294851504125a9fe
safibaber <safibaber@gmail.com>
2024-05-01 14:28:35 +0100

    premier depot

---
+++ src/Controller/PrincipalController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class PrincipalController extends AbstractController
+{
+    #[Route('/principal', name: 'app_principal')]
+    public function index(): Response
+    {
+        return $this->render('principal/index.html.twig', [
+            'controller_name' => 'PrincipalController',
+        ]);
+    }
+
+    #[Route('/', name: 'app_accueil')]
+    public function accueil(): Response
+    {
+        $message="Accueil";
+        return $this->render('model.html.twig',['message'=>$message]);
+    }
+
+    #[Route('/apropos', name: 'app_apropos')]
+    public function apropos(): Response
+    {
+        $message="à propos";
+        return $this->render('model.html.twig',['message'=>$message]);
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+        $message="page contact";
+        return $this->render('model.html.twig',['message'=>$message]);
+    }
+
+    #[Route('/services', name: 'app_services')]
+    public function services(): Response
+    {
+        $message="page services";
+        return $this->render('model.html.twig',['message'=>$message]);
+    }
+}
---
+++ src/Entity/Task.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TaskRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TaskRepository::class)]
+class Task
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $created = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $completed = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $updated = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getCreated(): ?\DateTimeInterface
+    {
+        return $this->created;
+    }
+
+    public function setCreated(\DateTimeInterface $created): static
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    public function getCompleted(): ?\DateTimeInterface
+    {
+        return $this->completed;
+    }
+
+    public function setCompleted(\DateTimeInterface $completed): static
+    {
+        $this->completed = $completed;
+
+        return $this;
+    }
+
+    public function getUpdated(): ?\DateTimeInterface
+    {
+        return $this->updated;
+    }
+
+    public function setUpdated(\DateTimeInterface $updated): static
+    {
+        $this->updated = $updated;
+
+        return $this;
+    }
+}
